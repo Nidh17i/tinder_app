02e57f92af84a770ebf126fa26826ff702d0eb58
Nidh17i <nidarwade@bestpeers.in>
2025-12-03 08:58:53 +0530

    .

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './pages/Navbar';
 import { Home } from './pages/Home';
 import { AllNetwork } from './components/AllNetwork';
 import { NotFound } from './components/NotFound';
+import { ProtectedRoute } from './components/ProtectedRoute';
 
 function App() {
 
@@ -17,9 +18,11 @@ function App() {
      <Route path='/signup' element={<Signup/>}/>
      <Route path='/login' element={<Login/>}/>
      
-
-     <Route path='/'element={<Home/>}/>
+    <Route element={<ProtectedRoute/>}>
+    <Route path='/'element={<Home/>}/>
      <Route path='/myNetwork' element={<AllNetwork/>}/>
+    </Route>
+     
      <Route path='*' element={<NotFound/>}/>
       </Routes>
       </BrowserRouter>
