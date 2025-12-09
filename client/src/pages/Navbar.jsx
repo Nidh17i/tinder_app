3990b9fc62f41339b388493a0d02936e3846c14f
Nidh17i <nidarwade@bestpeers.in>
2025-12-09 14:27:13 +0530

    .

--- client/src/pages/Navbar.jsx
+++ client/src/pages/Navbar.jsx
@@ -105,12 +105,12 @@ export default function Navbar() {
                 >
                   Login
                 </Link>
-                <Link
+                {/* <Link
                   to="/signup"
                   className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-full"
                 >
                   Signup
-                </Link>
+                </Link> */}
               </>
             )}
 
